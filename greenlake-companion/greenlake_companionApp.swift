8b77f4c752d565a5e8fbf2379f52b78ba35a0cfa
mrevanzak <mrevanzak@gmail.com>
2025-08-21 09:59:51 +0700

    Initial Commit

---
+++ greenlake-companion/greenlake_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  greenlake_companionApp.swift
+//  greenlake-companion
+//
+//  Created by Revanza Kurniawan on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct greenlake_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
